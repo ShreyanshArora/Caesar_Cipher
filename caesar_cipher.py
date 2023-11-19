310db584ab6f1df13e6676f64ba026b6fbf26911
Shreyansh Arora <144546852+ShreyanshArora@users.noreply.github.com>
2023-11-19 17:15:04 +0530

    Caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,36 +1,37 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
+print(logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text,shift_amount):
-  cipher_text=""
-  for letter in plain_text:
-    position=alphabet.index(letter)
-    new_position=position+shift_amount  
-    new_letter=alphabet[new_position]
-    cipher_text+=new_letter
-  print(f"The encoded text is {cipher_text}")
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-def decrypt(cipher_text,shift_number):
-  plain_text=""
- 
-  for letter in cipher_text:
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
     
-    position=alphabet.index(letter)
-    new_positionn=position-shift_number
-    new_letter=alphabet[new_positionn]
-    plain_text+=new_letter
-  print(f"The decoded text is {plain_text}")
-
-
-if direction=="encode":
-  encrypt(plain_text=text,shift_amount=shift)
-elif direction=="decode":
-  decrypt(cipher_text=text,shift_number=shift)
-
 
 
 
